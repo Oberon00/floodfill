44fb6beafeacc80bd775d9f635458f316d5e9e98
Christian Neumüller <cn00@gmx.at>
2012-09-22 18:57:42 +0200

    Fixed position handling in GraphicComponent.
    Renamed GraphicComponent.drawable to ._drawable and added :drawable() and :setDrawable().
    The setter sets the position of the drawable.
    
    :initComponent() now also sets the initial position of the drawable.

--- game/lua/comp/Graphic.lua
+++ game/lua/comp/Graphic.lua
@@ -1,20 +1,38 @@
 local C = component 'GraphicComponent'
 
-function C:_init(drawable)
-	self.drawable = drawable
+function C:init(drawable)
+	self._drawable = drawable
+end
+
+function C:drawable()
+	return self._drawable
+end
+
+function C:setDrawable(drawable)
+	self._drawable = drawable
+	if drawable and self.pos then
+		self._drawable.position = self.pos.position
+	end
 end
 
 function C:initComponent()
-	local pos = self.parent:require 'PositionComponent'
-	self.evts:connect(pos, "rectChanged", function(r)
-		if self.drawable then
-			self.drawable.position = r.position
+	self.pos = self.parent:require 'PositionComponent'
+	if self._drawable then
+		self._drawable.position = self.pos.position
+	end
+	self.evts:connect(self.pos, "rectChanged", function(r)
+		if self._drawable then
+			self._drawable.position = r.position
 		end
 	end)
 end
 
 function C:cleanup()
-	if self.drawable then
-		self.drawable:release()
+	self.pos = nil
+	if self._drawable then
+		self._drawable:release()
+		self._drawable = nil
 	end
 end
+
+return C
