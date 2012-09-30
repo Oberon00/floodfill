e92701b4d1386e23772064370394a99a5d4ac648
Christian Neumüller <cn00@gmx.at>
2012-09-30 16:17:06 +0200

    comp.CollisionInfo: Now takes jd.Vec2.
    +checkVec(v, ok)
    Check if there is more than one direction per vector, e.g. jd.Vec2(1,1) is
    's' and 'e'.

--- game/lua/comp/CollisionInfo.lua
+++ game/lua/comp/CollisionInfo.lua
@@ -2,16 +2,34 @@ local C = component 'CollisionInfoComponent'
 
 local util = require 'util'
 
+local function checkVec(v, ok)
+	if type(v) == 'string' then
+		return ok[v]
+	end
+	local result = true
+	if v.x < 0 then
+		result = result and ok.w
+	elseif v.x > 0 then
+		result = result and ok.e
+	end
+	if v.y < 0 then
+		result = result and ok.n
+	elseif v.y > 0 then
+		result = result and ok.s
+	end
+	return result
+end
+
 function C:init(tile)
 	self.tile = tile
 end
 
 function C:canEnter(entity, dir, from, to)
-	return self.tile.enterable[dir]
+	return checkVec(dir, self.tile.enterable)
 end
 
 function C:canLeave(entity, dir, from, to)
-	return self.tile.leaveable[dir]
+	return checkVec(dir, self.tile.leaveable)
 end
 
 return C
