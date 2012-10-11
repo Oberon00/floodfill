c0772daff33aebe0d09b161126109c7cece65000
Christian Neumüller <cn00@gmx.at>
2012-10-11 20:07:15 +0200

    configuration.lua: Disable vsynch.

--- game/configuration.lua
+++ game/configuration.lua
@@ -2,7 +2,7 @@ return {
 
     video = {
         mode       = jd.VideoMode(800, 600),
-        vsync      = true,
+        vsync      = false,
         fullscreen = false,
         framelimit = 240
     },
