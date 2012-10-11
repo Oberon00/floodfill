d681c6c86cea09ccf9578c03993bdf07dfa4a9d9
Christian Neumüller <cn00@gmx.at>
2012-10-11 20:09:50 +0200

    states.Game: stop(): Kill entities.
    Also remove self.tilemap. It is already contained in self.world.

--- game/lua/states/Game.lua
+++ game/lua/states/Game.lua
@@ -5,13 +5,23 @@ oo.cppclass('Game', jd.State)
 
 function Game:prepare()
 	local maplayer = jd.drawService:layer(2)
-	self.tilemap = jd.Tilemap(maplayer.group)
-	self.world = loadMap(self.tilemap, "level1")
-	maplayer.view.rect = self.tilemap.bounds
+	local tilemap = jd.Tilemap(maplayer.group)
+	self.world = loadMap(tilemap, "level1")
+	maplayer.view.rect = tilemap.bounds
 end
 
 function Game:stop()
-	self.tilemap = nil
+	for _, entity in pairs(self.world.tileProxies) do
+		entity:kill()
+	end
+	for _, entity in ipairs(self.world.substituteObjects) do
+		entity:kill()
+	end
+	for _, group in pairs(self.world.mapObjects) do
+		for _, entity in ipairs(group) do
+			entity:kill()
+		end
+	end
 	self.world = nil
 end
 
