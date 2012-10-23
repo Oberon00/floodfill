5b99e6ea7dbed6f4e6a3d985936db8aa262f4f83
Christian Neumüller <cn00@gmx.at>
2012-10-23 16:30:23 +0200

    evt: Add Signal and Connection.

--- game/lua/lib/evt.lua
+++ game/lua/lib/evt.lua
@@ -6,6 +6,7 @@
 
 local tabutil = require 'tabutil'
 local util = require 'util'
+local oo = require 'oo'
 
 local M = { }
 
@@ -110,5 +111,123 @@ lclass('Table', M)
 		end
 		self.evts = { }
 	end
+
+local Connection = oo.cppclass('Connection', oo.NIL_ENV, jd.ConnectionBase)
+	
+lclass('Signal', M)
+
+	local function erase(self, i)
+		local slotCount = #self
+		if i == slotCount then
+			self[i] = nil
+		else
+			self[i] = self[slotCount]
+			self[slotCount] = nil
+		end
+	end
+
+	local function disconnectHint(self, f, hint)
+		if self[i] == f then
+			erase(self, i)
+			return true
+		end
+		return self:disconnect(f)
+	end
+	
+	--[[
+		Call all connected slots with the passed parameters. Note that the order
+		in which the slots are called is undefined.
+	--]]
+	function M.Signal:__call(...)
+		for i = 1, #self do
+			self[i](...)
+		end
+	end
+	
+	--[[
+		Call, collecting the return values of the signals.
+		This function will return a table with one table per connected function
+		(slot) each obtained by packing the return value of the slot with
+		table.pack(): { table.pack(slot1(...)), table.pack(slot2(...)), ... }
+		
+		Compared to the __call metamethod, this function is very slow, so you
+		should use it only if you really need the return values.
+	--]]
+	function M.Signal:callR(...)
+		local result = { }
+		for i = 1, #self do
+			result[i] = table.pack(self[i](...))
+		end
+		return result
+	end
+	
+	function M.Signal:connect(f)
+		local i = #self + 1
+		self[i] = f
+		return Connection(self, f, i)
+	end
+	
+	--[[
+		Returns true if f was disconnected and false otherwise (if f is not
+		connected).
+	--]]
+	function M.Signal:disconnect(f)
+		local slotCount = #self
+		for i = 1, slotCount do
+			if self[i] == f then
+				if i == slotCount then
+					self[i] = nil
+				else
+					self[i] = self[slotCount]
+					self[slotCount] = nil
+				end
+				return true
+			end
+		end
+		return false
+	end
+	
+	function M.Signal.create(...)
+		local names = {...}
+		local result = { }
+		for i = 1, #names do
+			result[names[i]] = M.Signal()
+		end
+		return result
+	end
+	
+	function M.Signal.createInComponent(component, ...)
+		local result = M.Signal.create(...)
+		component._signals = result
+		return result
+	end
+	
+function Connection:__init(sig, f, hint)
+	jd.ConnectionBase.__init(self)
+	assert(sig and f and hint)
+	self.sig, self.f, self.hint = sig, f, hint
+end
+
+function Connection:getIsConnected()
+	return not not self.sig
+end
+
+function Connection:disconnect()
+	local success = disconnectHint(self.sig, self.f, self.hint)
+	assert(success)
+	self.sig, self.f, self.hint = nil, nil, nil
+end
+
+
+			
+function jd.callback_connectLuaEvent(component, name, receiver)
+	if type(component._signals) == 'table' then
+		local sig = component._signals[name]
+		if sig then
+			return sig:connect(receiver)
+		end
+	end
+	return nil
+end
 
 return M
