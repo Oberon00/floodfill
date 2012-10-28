91accca0af3d837266adb313778ab05181b00dc1
Christian Neumüller <cn00@gmx.at>
2012-10-28 19:52:51 +0100

    states.Game: Support winning a level and starting next.

--- game/lua/states/Game.lua
+++ game/lua/states/Game.lua
@@ -7,19 +7,36 @@ local Level = require 'Level'
 
 oo.cppclass('GameState', jd.State)
 
+local maplayer = jd.drawService:layer(2)
+
+local nextLevel
+
+local function startLevel(self)
+	self.level = Level(self.levels:currentLevel().name, maplayer.group)
+	evt.connectToKeyPress(jd.kb.F5, function() self.level:restart() end)
+	self.level:start()
+	maplayer.view.rect = self.level.world.map.bounds
+	function self.level.world.winLevel()
+		nextLevel(self)
+	end
+end
+
+local function nextLevel(self)
+	evt.connectToKeyPress(jd.kb.F5, nil)
+	self.level:stop()
+	if not self.levels:advance() then
+		self.levels.currentIndex = 1
+	end
+	startLevel(self)
+end
+
 function GameState:__init()
 	jd.State.__init(self)
 	self.levels = LevelList(levelnamelist)
 end
 
 function GameState:prepare()
-	local maplayer = jd.drawService:layer(2)
-	
-	self.level = Level(self.levels:currentLevel().name, maplayer.group)
-	evt.connectToKeyPress(jd.kb.F5, function() self.level:restart() end)
-	self.level:start()
-	maplayer.view.rect = self.level.world.map.bounds
-
+	startLevel(self)
 end
 
 function GameState:stop()
