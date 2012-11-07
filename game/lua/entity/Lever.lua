08b25c7f4f59bbf834e0dac97105fd01f1551ce2
Christian Neumüller <cn00@gmx.at>
2012-11-07 13:23:46 +0100

    entity.Lever: Adjust to new Lock entity, use entity.TriggerBase.
    Also use maploader.findConnections().

--- game/lua/entity/Lever.lua
+++ game/lua/entity/Lever.lua
@@ -1,82 +1,34 @@
-require 'comp.Activateable'
-require 'comp.CollisionInfo'
 local layers = require 'data.layers'
+local TriggerBase = require 'entity.TriggerBase'
 
 local M = { }
 
-local LAYER_LOCK = layers.LOCKS
-local OBJT_LINE = jd.mapInfo.Object.LINE
-
 local function activate(self, activator)
-	self.data.opened = not self.data.opened
-	self.data:apply()
-end
-
-local function apply(self)
-	if self.lockp then
-		self.map:set(
-			self.lockp, self.opened and self.TID_OPEN or self.lockedTid)
+	self.data.on = not self.data.on
+	self.data.map:set(self.data.pos.tilePosition,
+		self.data.on and self.data.TID_ON or self.data.TID_OFF)
+	local cons = self.data.connections
+	for i = 1, #cons  do
+		local s = self.data.on
+		if cons[i].negate then
+			s = not s
+		end
+		cons[i].dst:setState(s)
 	end
-	self.map:set(self.selfp, self.opened and self.TID_ON or self.TID_OFF)
 end
 
 function M.createSubstitute(name, id, pos, data, props)
-	local tiles = require 'data.tiles' 
 	local tids = data.tileMapping.byName
 	
 	local TID_ON  = tids['lever#2']
 	local TID_OFF = tids['lever#1']
-
-	local entity = jd.Entity()
-	CollisionInfoComponent(entity, tiles[name])
-	local poscomp = jd.PositionComponent(entity)
-	poscomp.rect = data.map:tileRect(jd.Vec2(pos.x, pos.y))
-	jd.TilePositionComponent(entity, data.map, pos.z)
-	jd.TileCollisionComponent(entity, data.tileCollisionInfo)
 	
-	local actData = {
-		apply = apply,
-		
-		opened = false,
-		lockedTid = false,
-		selfp = pos,
-		lockp = false,
-		map = data.map,
-		TID_OPEN = tids.lock_open,
-		TID_ON = TID_ON,
-		TID_OFF = TID_OFF
-	}
+	local entity, actcomp = TriggerBase.createSubstitute(
+		activate, layers.LOCKS, name, id, pos, data, props)
 	
-	local connections = props.objectGroups:get 'connections'
-	if not connections then
-		jd.log.w "Lever.substitute: object group 'connections' missing"
-		return nil
-	end
-
-	local r = data.map:localTileRect(jd.Vec2(pos.x, pos.y))
-
-	for c in connections.objects:iter() do
-		if c.objectType == OBJT_LINE then
-			local points = c.absolutePoints
-			local p1 = points:get(1)
-			local p2 = points:get(points.count)
-			local lockp = r:contains(p1) and p2 or r:contains(p2) and p1
-			if lockp then
-				lockp = data.map:tilePosFromLocal(lockp)
-				actData.lockp = jd.Vec3(lockp.x, lockp.y, LAYER_LOCK)
-				local locktid = data.map:get(actData.lockp)
-				actData.lockedTid = locktid == tids.lock_open and
-					tids.lock_nesw or locktid
-				actData.opened = id == TID_ON
-				break
-			end -- if lockp
-		end -- if c.objectType == OBJT_LINE
-	end -- for each c in connections
-	
-	ActivateableComponent(entity, activate, actData)
-	entity:finish()
-
-	actData:apply()
+	actcomp.data.on = id == TID_ON
+	actcomp.data.TID_ON = TID_ON
+	actcomp.data.TID_OFF = TID_OFF
 	
 	return entity
 end -- function M.substitute
---
+++ game/lua/entity/TriggerBase.lua
@@ -0,0 +1,55 @@
+require 'comp.Activateable'
+require 'comp.CollisionInfo'
+
+local M = { }
+
+function M.createSubstitute(onActivate, dstLayer, name, id, pos, data, props)
+	local maploader = require 'maploader'
+	local tiles = require 'data.tiles' 
+	
+	local entity = jd.Entity()
+	CollisionInfoComponent(entity, tiles[name])
+	local poscomp = jd.PositionComponent(entity)
+	
+	local map = data.map
+	poscomp.rect = map:tileRect(jd.Vec2(pos))
+	local tposcomp = jd.TilePositionComponent(entity, map, pos.z)
+	jd.TileCollisionComponent(entity, data.tileCollisionInfo)
+	
+	local actData = {
+		pos = tposcomp,
+		map = data.map,
+		connections = { }
+	}
+	
+	local connections = props.objectGroups:get 'connections'
+	if not connections then
+		jd.log.w "TriggerBase.createSubstitute: group 'connections' missing"
+		return nil
+	end
+	local conobjs, points = maploader.findConnections(
+		poscomp.rect, map, connections.objects)
+	local actCons = actData.connections
+	data.postLoad:connect(function(data)
+		for i = 1, #points do
+			p = data.map:tilePosFromGlobal(points[i])
+			local dst = data.tileCollisionInfo:colliding(
+				jd.Vec3(p.x, p.y, dstLayer))
+			if dst then
+				dst = dst.parent:component 'TwoStateTileComponent'
+				if dst then
+					actCons[#actCons + 1] = {
+						dst = dst,
+						negate = not not conobjs[i].properties:get 'negate'
+					}
+				end -- if dst (TwoStateTileComponent)
+			end -- if dst
+		end -- for each in points
+	end) -- data.postLoad callback
+	local actcomp = ActivateableComponent(entity, onActivate, actData)
+	entity:finish()
+	
+	return entity, actcomp
+end -- function M.substitute
+
+return M
