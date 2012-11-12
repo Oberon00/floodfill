7d06f92ad19604b1d18432e8e5163d93388f6078
Christian Neumüller <cn00@gmx.at>
2012-11-12 20:13:19 +0100

    +entity.BridgeBase: Split from entity.LowBridge

---
+++ game/lua/entity/BridgeBase.lua
@@ -0,0 +1,24 @@
+require 'comp.CollisionInfo'
+local Flood = require 'proc.Flood'
+
+local M = { }
+
+function M.createSubstitute(canEnter, name, id, pos, data, props)
+	local tiles = require 'data.tiles' 
+	local entity = jd.Entity()
+	local cinfo = CollisionInfoComponent(entity, tiles[name])
+	local poscomp = jd.PositionComponent(entity)
+	poscomp.rect = data.map:tileRect(jd.Vec2(pos))
+	local tposcomp = jd.TilePositionComponent(entity, data.map, pos.z)
+	jd.TileCollisionComponent(entity, data.tileCollisionInfo)
+	local oldCanEnter = cinfo.canEnter
+	local flood = Flood.ensureFlood(data)
+	function cinfo.canEnter(self, entity, ...)
+		return canEnter(flood:isFlooded(tposcomp.tilePosition))
+		       and oldCanEnter(self, entity, ...)
+	end
+	entity:finish()
+	return entity
+end -- function M.substitute
+
+return M
--- game/lua/entity/LowBridge.lua
+++ game/lua/entity/LowBridge.lua
@@ -1,24 +1,13 @@
-require 'comp.CollisionInfo'
-local Flood = require 'proc.Flood'
+local BridgeBase = require 'entity.BridgeBase'
 
 local M = { }
 
-function M.createSubstitute(name, id, pos, data, props)
-	local tiles = require 'data.tiles' 
-	local entity = jd.Entity()
-	local cinfo = CollisionInfoComponent(entity, tiles[name])
-	local poscomp = jd.PositionComponent(entity)
-	poscomp.rect = data.map:tileRect(jd.Vec2(pos))
-	local tposcomp = jd.TilePositionComponent(entity, data.map, pos.z)
-	jd.TileCollisionComponent(entity, data.tileCollisionInfo)
-	local canEnter = cinfo.canEnter
-	local flood = Flood.ensureFlood(data)
-	function cinfo.canEnter(self, entity, ...)
-		return not flood:isFlooded(tposcomp.tilePosition)
-		       and canEnter(self, entity, ...)
-	end
-	entity:finish()
-	return entity
+function M.createSubstitute(...)
+	return BridgeBase.createSubstitute(
+		function(flooded)
+			return not flooded
+		end,
+		...)
 end -- function M.substitute
 
 return M
