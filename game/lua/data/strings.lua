1b1243cbda7d0f0a6fb6b6cd72b7ccbe6d5f3ea1
Christian Neumüller <cn00@gmx.at>
2012-11-22 21:46:28 +0100

    Add data.strings to ease localization.

---
+++ game/lua/data/strings.lua
@@ -0,0 +1,43 @@
+--[[
+	module data.strings
+	language: German
+--]]
+
+return {
+
+-- State: Splash
+	press_any_key = "Drücken Sie eine beliebige Taste . . .",
+
+-- State: Menu
+	menu_back = "ZURÜCK",
+	
+	-- Main menu
+	new_game = "Neues Spiel",
+	continue_game = "Spiel fortsetzen",
+	show_credits = "Credits",
+	exit_program = "Beenden",
+	
+-- State: Game
+	level_i = "Level %i",
+	
+-- Debugging
+	_test1 = "@strings._test1",
+	_test2 = "@strings._test2",
+	_test3 = "@strings._test3",
+	_test4 = "@strings._test4",
+	_test5 = "@strings._test5",
+	_test6 = "@strings._test6",
+	_test7 = "@strings._test7",
+	_test8 = "@strings._test8",
+	_test9 = "@strings._test9",
+	_test10 = "@strings._test10",
+	_test11 = "@strings._test11",
+	_test12 = "@strings._test12",
+	_test13 = "@strings._test13",
+	_test14 = "@strings._test14",
+	_test15 = "@strings._test15",
+	_test16 = "@strings._test16",
+	_test17 = "@strings._test17",
+	_test18 = "@strings._test18",
+	_test19 = "@strings._test19",
+}
