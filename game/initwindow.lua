419cad43ab355e788b19946effd92176a60b74e5
Christian Neumüller <cn00@gmx.at>
2013-01-17 13:38:47 +0100

    initwindow: Allow reapplying configuration.

--- game/initwindow.lua
+++ game/initwindow.lua
@@ -1,15 +1,22 @@
-local window = jd.RenderWindow(
-    jd.conf.video.mode or jd.VideoMode(800, 600),
-    jd.conf.misc.title or "Jade Engine",
-    jd.conf.video.fullscreen and
-        bit32.bor(jd.Window.STYLE_DEFAULT, jd.Window.STYLE_FULLSCREEN) or
-        jd.Window.STYLE_DEFAULT)
-window:setVSyncEnabled(jd.conf.video.vsync)
-if jd.conf.misc.iconFilename then
-    window:setIcon(jd.Image.request(jd.conf.misc.iconFilename))
+local function reinitialize(window)
+    window:create(
+        jd.conf.video.mode or jd.VideoMode(800, 600),
+        jd.conf.misc.title or "Jade Engine",
+        jd.conf.video.fullscreen and
+            bit32.bor(jd.Window.STYLE_DEFAULT, jd.Window.STYLE_FULLSCREEN) or
+            jd.Window.STYLE_DEFAULT)
+    window:setVSyncEnabled(jd.conf.video.vsync)
+    window:setFramerateLimit(jd.conf.video.framelimit or 0)
+    if jd.conf.misc.iconFilename then
+        window:setIcon(jd.Image.request(jd.conf.misc.iconFilename))
+    end
 end
-window:setFramerateLimit(jd.conf.video.framelimit or 0)
 
+
+local window = jd.RenderWindow()
+reinitialize(window)
+
+window.reinitialize = reinitialize
 jd.window = window
 
 return window
