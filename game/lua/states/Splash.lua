41fedfcc5e11c4abe28056db67d4b57b1d06b202
Christian Neumüller <cn00@gmx.at>
2013-06-12 20:24:20 +0200

    states.Splash: Fix bug when skipIntro() is triggered by world.winLevel().
    Also replace tabs with spaces.

--- game/lua/states/Splash.lua
+++ game/lua/states/Splash.lua
@@ -1,32 +1,39 @@
 local oo = require 'oo'
 local Level = require 'Level'
 local ContinueScreen = require 'ContinueScreen'
+local text = require 'text'
 
 local C = oo.cppclass('SplashState', jd.State)
 
-local CONT_TEXT_PADDING = 20
 
 local maplayer = jd.drawService:layer(2)
 
 local function skipIntro()
-	return jd.stateManager:switchTo('Menu')
+    -- Switch state in next frame, otherwise, if this is called from
+    -- proc.Flood.onFlow, it disconnects while calling the signal, which leads
+    -- to a crash.
+    local con
+    con = jd.connect(jd.mainloop, 'preFrame', function()
+        jd.stateManager:switchTo('Menu')
+        con:disconnect()
+    end)
 end
 
 function C:prepare()
-	self.screen = ContinueScreen()
-	self.screen:show(skipIntro)
-	self.screen.continueText.color = jd.Color(60, 255, 100, 200)
-	self.level = Level(jd.conf.misc.splashLevel, maplayer.group)
-	self.level:start()
-	maplayer.view.rect = self.level.world.map.bounds
-	self.level.world.winLevel = skipIntro
+    self.screen = ContinueScreen()
+    self.screen:show(skipIntro)
+    self.screen.continueText.color = jd.Color(60, 255, 100, 200)
+    self.level = Level(jd.conf.misc.splashLevel, maplayer.group)
+    self.level:start()
+    maplayer.view.rect = self.level.world.map.bounds
+    self.level.world.winLevel = skipIntro
 end
 
 function C:pause()
-	self.level:stop()
-	self.level = nil
-	self.screen:reset()
-	self.screen = nil
+    self.level:stop()
+    self.level = nil
+    self.screen:reset()
+    self.screen = nil
 
 end
 
