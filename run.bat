2edf4185b1eb0bbfd94333bd55f1c4ca6438b1cb
Christian Neumüller <cn00@gmx.at>
2013-06-25 13:15:58 +0200

    Remove obsolete launcher files.

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
 @echo off
 echo jd is starting...
-cd %~dp0\game
-..\..\build11\src\Debug\jd.exe || goto :err
+pushd %~dp0\game
+%JD_ROOT%\bin\jd.exe || goto :err
 
 goto :EOF
 
 :err
 echo Exitcode: %ERRORLEVEL%
+
+popd
--- runr.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo jd is starting...
-cd %~dp0\game
-..\..\build11\src\RelWithDebInfo\jd.exe || goto :err
-
-goto :EOF
-
-:err
-echo Exitcode: %ERRORLEVEL%
